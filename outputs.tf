043d5df73336e3b603d368d568c3510af73052e1
Dheimy <jheimyeng@hotmail.com>
2025-05-28 21:55:15 -0300

    add vpc endpoint

--- outputs.tf
+++ outputs.tf
@@ -36,4 +36,34 @@ output "public_route_table_id" {
 output "private_route_table_ids" {
   description = "List of IDs of private route tables"
   value       = aws_route_table.private[*].id
+}
+
+output "cloudwatch_logs_endpoint_id" {
+  description = "The ID of the CloudWatch Logs VPC endpoint"
+  value       = var.enable_cloudwatch_logs_endpoint ? aws_vpc_endpoint.cloudwatch_logs[0].id : null
+}
+
+output "cloudwatch_monitoring_endpoint_id" {
+  description = "The ID of the CloudWatch Monitoring VPC endpoint"
+  value       = var.enable_cloudwatch_monitoring_endpoint ? aws_vpc_endpoint.cloudwatch_monitoring[0].id : null
+}
+
+output "kms_endpoint_id" {
+  description = "The ID of the KMS VPC endpoint"
+  value       = var.enable_kms_endpoint ? aws_vpc_endpoint.kms[0].id : null
+}
+
+output "ecr_public_endpoint_id" {
+  description = "The ID of the ECR Public VPC endpoint"
+  value       = var.enable_ecr_public_endpoint ? aws_vpc_endpoint.ecr_public[0].id : null
+}
+
+output "stepfunctions_endpoint_id" {
+  description = "The ID of the Step Functions VPC endpoint"
+  value       = var.enable_stepfunctions_endpoint ? aws_vpc_endpoint.stepfunctions[0].id : null
+}
+
+output "lambda_endpoint_id" {
+  description = "The ID of the Lambda VPC endpoint"
+  value       = var.enable_lambda_endpoint ? aws_vpc_endpoint.lambda[0].id : null
 } 
