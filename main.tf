5e225146d014371b81995c769803f9e6fb3e9fa0
Dheimy <jheimyeng@hotmail.com>
2025-05-28 22:22:43 -0300

    ajust vpc

--- main.tf
+++ main.tf
@@ -22,40 +22,6 @@ resource "aws_vpc" "main" {
   )
 }
 
-resource "aws_subnet" "public" {
-  count                   = length(var.public_subnets)
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnets[count.index]
-  availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = true
-
-  tags = merge(
-    var.tags,
-    {
-      Name        = "${var.vpc_name}-public-${count.index}"
-      Environment = var.environment
-      Project     = var.project_name
-    }
-  )
-}
-
-resource "aws_subnet" "private" {
-  count                   = length(var.private_subnets)
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.private_subnets[count.index]
-  availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = false
-
-  tags = merge(
-    var.tags,
-    {
-      Name        = "${var.vpc_name}-private-${count.index}"
-      Environment = var.environment
-      Project     = var.project_name
-    }
-  )
-}
-
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
--- subnets.tf
+++ subnets.tf
@@ -18,6 +18,7 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnets[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
 
   tags = merge(
     var.tags,
