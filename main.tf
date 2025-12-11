2c2bc3758ce83cb0ba8033fe083d0bbf6cf372b1
Dheimy <33067495+dheimy00@users.noreply.github.com>
2025-12-11 20:08:30 -0300

    Remove Terraform provider configuration
    Removed Terraform required providers block.

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = var.enable_dns_hostnames
