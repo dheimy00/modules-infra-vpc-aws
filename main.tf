352f68149cfdc100d8621eb70299fdb14640b352
Dheimy <jheimyeng@hotmail.com>
2025-12-11 20:09:58 -0300

    feat: deletando terraform versao

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = var.enable_dns_hostnames
@@ -412,4 +403,4 @@ resource "aws_security_group" "vpc_endpoints" {
 }
 
 # Get current region
-data "aws_region" "current" {} 
+data "aws_region" "current" {}
--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,12 @@ output "vpc_cidr_block" {
   value       = aws_vpc.main.cidr_block
 }
 
-output "public_subnet_ids" {
+output "public_subnets" {
   description = "List of IDs of public subnets"
   value       = aws_subnet.public[*].id
 }
 
-output "private_subnet_ids" {
+output "private_subnets" {
   description = "List of IDs of private subnets"
   value       = aws_subnet.private[*].id
 }
